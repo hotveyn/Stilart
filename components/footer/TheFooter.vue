84177570e126535191c470a14ce982d793583986
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-10 03:02:26 +0300

    footer

--- components/footer/TheFooter.vue
+++ components/footer/TheFooter.vue
@@ -1,7 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SectionContacts from '~/components/Section/SectionContacts.vue'
+import BaseNavigation from '~/components/Base/BaseNavigation.vue'
+</script>
 
 <template>
-  <h2>footer</h2>
+  <footer class="footer w">
+    <SectionContacts class="footer__contacts" />
+    <div class="container footer__content">
+      <NuxtLink to="/">
+        <img
+          src="/images/logo/logoB.svg"
+          alt="logo"
+        >
+      </NuxtLink>
+      <BaseNavigation type="footer" class="footer__nav" />
+    </div>
+  </footer>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.footer {
+  margin-top: 120px;
+  &__content {
+    padding: 20px 0px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
