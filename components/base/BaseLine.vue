7b5772d6a54d4c6d0f8803cf9f16e286ffc68699
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-11 02:59:46 +0300

    Create BaseLine.vue

---
+++ components/base/BaseLine.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { reactive } from '#imports'
+
+const props = defineProps<{
+  top?: string
+  left?: string
+  right?: string
+  bottom?: string
+  width?: string
+  height?: string
+  color: 'wood' | 'white'
+}>()
+
+const style = reactive({
+  top: props.top,
+  left: props.left,
+  right: props.right,
+  bottom: props.bottom,
+  width: props.width,
+  height: props.height
+})
+</script>
+
+<template>
+  <span :class="[`base-line_${color}`]" :style="style" class="base-line" />
+</template>
+
+<style scoped lang="scss">
+.base-line {
+  opacity: .5;
+  display: block;
+  min-height: 1px;
+  min-width: 1px;
+  position: absolute;
+  z-index: 0;
+
+  &_wood {
+    background-color: $tree;
+  }
+
+  &_white {
+    background-color: $white;
+  }
+}
+</style>
