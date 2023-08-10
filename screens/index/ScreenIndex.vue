eee7e843d0869e0dc8859fe99c0eb1420ee97a86
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-10 03:03:03 +0300

    screens

---
+++ screens/index/ScreenIndex.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import SectionAbout from '~/components/Section/SectionAbout.vue'
+import SectionProjects from '~/components/Section/SectionProjects.vue'
+import SectionServices from '~/components/Section/SectionServices.vue'
+import SectionStages from '~/components/Section/SectionStages.vue'
+import SectionFAQ from '~/components/Section/SectionFAQ.vue'
+</script>
+
+<template>
+  <div class="index-screen">
+    <SectionAbout />
+    <SectionProjects />
+    <SectionServices />
+    <SectionStages />
+    <SectionFAQ />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.index-screen {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 120px;
+}
+</style>
