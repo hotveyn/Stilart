a664d08d351a7bc99af2a2a1c4e9546f8941c1f7
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-10 03:02:33 +0300

    header

--- components/header/TheHeader.vue
+++ components/header/TheHeader.vue
@@ -1,39 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseNavigation from '~/components/Base/BaseNavigation.vue'
+import TheOpening from '~/components/header/sections/SectionOpening.vue'
+</script>
 
 <template>
-  <header class="header w">
-    <div class="container header__content">
-      <img src="/images/logo/logo.svg" alt="" class="header__logo" />
-      <nav class="header__nav nav">
-        <ul class="nav__list">
-          <li class="nav__item">
-            <a href="" class="nav__a">О нас</a>
-          </li>
-          <li class="nav__item">
-            <a href="" class="nav__a">Этапы работ</a>
-          </li>
-          <li class="nav__item">
-            <a href="" class="nav__a">Услуги</a>
-          </li>
-          <li class="nav__item">
-            <a href="" class="nav__a">Проекты</a>
-          </li>
-          <li class="nav__item">
-            <a href="" class="nav__a">Вопрос ответ</a>
-          </li>
-          <li class="nav__item">
-            <a href="" class="nav__a">Контакты</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
+  <div class="header-wrapper w">
+    <header class="header w">
+      <div class="container header__content">
+        <NuxtLink to="/">
+          <img
+            src="/images/logo/logoW.svg"
+            alt="logo"
+            class="header__logo"
+          >
+        </NuxtLink>
+        <BaseNavigation type="header" class="header__nav" />
+      </div>
+    </header>
+    <TheOpening />
+  </div>
 </template>
 
 <style scoped lang="scss">
+.header-wrapper{
+  background: linear-gradient(0deg, rgba(101, 83, 40, 0.50) 0%, rgba(101, 83, 40, 0.50) 100%), url('/images/bg/opening.png') no-repeat, lightgray 50% / cover no-repeat;
+  background-size: cover;
+  background-position: center;
+  margin-bottom: 120px;
+}
+
 .header {
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0);
+  border-bottom: 1px solid $white;
+  padding: 10px 0;
+  z-index: 2;
+
   &__content {
     display: flex;
     justify-content: space-between;
