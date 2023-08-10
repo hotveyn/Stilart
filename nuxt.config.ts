1dcf7e5d96d27f4693e107e3d5269b150bfc893f
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-11 02:59:37 +0300

    off pathPrefix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/google-fonts'],
+  components: {
+    pathPrefix: false
+  },
   vue: {
     defineModel: true
   },
