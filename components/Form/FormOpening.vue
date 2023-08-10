f10dfc4cfa14d867c1b343f609fad166103544c3
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-10 03:02:18 +0300

    Create FormOpening.vue

---
+++ components/Form/FormOpening.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const form = ref<HTMLInputElement | null>(null)
+const nameF = ref('')
+const mailF = ref('')
+function goMail () {
+  if (!form.value) { return }
+
+  if (form.value.checkValidity()) {
+    console.log('Form submitted successfully')
+  } else {
+    form.value.reportValidity()
+  }
+}
+</script>
+
+<template>
+  <form ref="form" class="form-opening w">
+    <p class="form-opening__title">
+      Оставте заявку
+    </p>
+    <BaseInput v-model.trim="nameF" required label-text="Имя" placeholder="Тихон" type="text" />
+    <BaseInput v-model.trim="mailF" required label-text="Электронная почта" placeholder="example@mail.ru" type="email" />
+    <BaseCheckbox required label-text="Согласие на обработку перс. данных" />
+    <BaseButton type="transparent" @click.prevent="goMail">
+      Отправить
+    </BaseButton>
+  </form>
+</template>
+
+<style scoped lang="scss">
+.form-opening {
+  padding: 40px;
+  background-color: $trnsp-tree;
+  backdrop-filter: blur(5px);
+  border-radius: 4px;
+  border: 1px solid $white;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  &__title {
+    font-size: 24px;
+    font-weight: 500;
+  }
+}
+</style>
