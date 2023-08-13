e2587f1fb5a84cca7dd121a446fc4cbb93cfca2c
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-13 18:48:54 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/google-fonts', 'nuxt-mail'],
+  components: {
+    pathPrefix: false
+  },
   vue: {
     defineModel: true
   },
